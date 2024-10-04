ff7a1a91cb4d2f0c229c1a3afe878a77b9e3f173
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2024-10-04 23:28:34 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,6 +42,9 @@
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+            <x-primary-button class="ms-3">
+                {{ __('r') }}
+            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
